77cb0fe61a3c087f3f9591346b80ada62dbd3cf0
sabrina <ragilsaputrisabrina@gmail.com>
2024-09-13 08:03:35 +0700

    route auth

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Auth::routes([
     'register' => false,
@@ -14,3 +12,14 @@ Auth::routes([
 ]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group([
+    'middleware' => ['auth'],
+    'prefix' => 'admin', //admin/tamu
+    'as' => 'admin.' //route('admin.')
+], function () {
+    //guestbook.test/admin ->route('admin.index')
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+    //guestbook.test/admin/dashborard ->route('admin.dashborard')
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+});
